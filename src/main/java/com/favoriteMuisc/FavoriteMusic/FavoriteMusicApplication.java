cd956a913c7bf737ce079c5aab4ee1a8c57a4d7d
Rafael Lima <rafa_xlr@hotmail.com>
2020-08-16 16:08:24 -0300

    First commit

---
+++ src/main/java/com/favoriteMuisc/FavoriteMusic/FavoriteMusicApplication.java
@@ -0,0 +1,14 @@
+package com.favoriteMuisc.FavoriteMusic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FavoriteMusicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FavoriteMusicApplication.class, args);
+		System.out.println("olá mundo");
+	}
+
+}
---
+++ src/test/java/com/favoriteMuisc/FavoriteMusic/FavoriteMusicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.favoriteMuisc.FavoriteMusic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FavoriteMusicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
